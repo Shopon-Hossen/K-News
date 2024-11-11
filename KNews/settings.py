b5cbff82fe26b13a1f7f41a37725a02d1598061e
Shopon Hossen <151064377+Shopon-Hossen@users.noreply.github.com>
2024-11-11 13:57:10 +0600

    1.2.1

--- KNews/settings.py
+++ KNews/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.3", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'KNews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
@@ -89,15 +89,15 @@ WSGI_APPLICATION = 'KNews.wsgi.application'
         'HOST': config('DB_HOST'),
         'PORT': '6543',
     }
-}"""
+}
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
