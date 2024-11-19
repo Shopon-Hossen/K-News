012d288ab3d78dbc90026f7dd690edb769135ea7
Shopon Hossen <151064377+Shopon-Hossen@users.noreply.github.com>
2024-11-19 14:36:43 +0600

    1.2.2

--- KNews/settings.py
+++ KNews/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'KNews.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -89,15 +89,15 @@ DATABASES = {
         'HOST': config('DB_HOST'),
         'PORT': '6543',
     }
-}
+}"""
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
